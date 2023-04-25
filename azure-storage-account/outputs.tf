80192e34c42f49e5f7245d751963b4d89c63778c
oginskis <viktors.oginskis@gmail.com>
2023-04-25 12:16:19 +0300

    Azure Storage Account component #1

--- azure-storage-account/outputs.tf
+++ azure-storage-account/outputs.tf
@@ -1,3 +1,4 @@
 output "storage_account_primary_access_key" {
-  value = nonsensitive(azurerm_storage_account.storage.primary_access_key)
+  sensitive = true
+  value = azurerm_storage_account.storage.primary_access_key
 }
